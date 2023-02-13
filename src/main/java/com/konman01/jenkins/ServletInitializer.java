0bff1b5b3a393e5b68a0c1d5ce78b89ac887aca4
konman01 <konimanjunatha@gmail.com>
2023-02-13 00:29:34 -0600

    Added Inital code

---
+++ src/main/java/com/konman01/jenkins/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.konman01.jenkins;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(JenkinsApplication.class);
+	}
+
+}
